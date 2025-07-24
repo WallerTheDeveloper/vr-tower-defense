c1b674594432629aa65b450237aebac5787c6c67
Daniel <golo7ov.danil@gmail.com>
2025-07-24 13:15:48 +0200

    Rocket Launcher adjustments

--- Assets/Scripts/Core/TowersBehaviour/RocketLauncherTower.cs
+++ Assets/Scripts/Core/TowersBehaviour/RocketLauncherTower.cs
@@ -1,19 +1,73 @@
+using Core.TowersBehaviour.States;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
 namespace Core.TowersBehaviour
 {
     public class RocketLauncherTower : Tower
     {
+        [SerializeField] private GameObject statesLayer;
+        
+        // Machine Gun States
+        private TowerAutoPlacement _autoPlacement;
+        private TowerRotate _towerRotate;
+        private TowerShooting _towerShooting;
+        
         protected override void Initialize()
         {
+            _autoPlacement = statesLayer.GetComponent<TowerAutoPlacement>();
+            _towerRotate = statesLayer.GetComponent<TowerRotate>();
+            _towerShooting = statesLayer.GetComponent<TowerShooting>();
+            
+            base.ChangeState(_autoPlacement);
+         
+            _autoPlacement.OnStateFinished += ChangeToRotationState;
+            _towerRotate.OnStateFinished += ChangeToShootingState;
+            _towerRotate.OnTargetFound += currentTarget =>
+            {
+                _towerShooting.SetTarget(currentTarget);
+            };
         }
 
         protected override void Tick()
         {
+            if (_autoPlacement.IsStateFinished && !_towerRotate.IsStateFinished)
+            {
+                _towerRotate.Tick();
+            }
+            if (_towerRotate.IsStateFinished)
+            {
+                _towerShooting.Tick();
+            }
         }
 
         protected override void FixedTick()
         {
+            if (!_autoPlacement.IsStateFinished)
+            { 
+                _autoPlacement.FixedTick();
+            }
         }
 
+        private void ChangeToRotationState()
+        {
+            _autoPlacement.OnStateFinished -= ChangeToRotationState;
+            
+            var grabbableObject = GetComponent<XRGrabInteractable>();
+            if (grabbableObject != null)
+            {
+                grabbableObject.enabled = false;
+            }
+            
+            base.ChangeState(_towerRotate);
+        }
+        
+        private void ChangeToShootingState()
+        {
+            _towerRotate.OnStateFinished -= ChangeToShootingState;
+            base.ChangeState(_towerShooting);
+        }
+        
         protected override void Deinitialize()
         {
         }
